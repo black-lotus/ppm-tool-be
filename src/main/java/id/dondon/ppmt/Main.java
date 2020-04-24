2587e766cf8d8fd9e5561cf9e6eb3b7492eb217e
purbayanto <romdoni.purbayanto@tiket.com>
2020-04-24 22:58:12 +0700

    enable cross origin

--- src/main/java/id/dondon/ppmt/Main.java
+++ src/main/java/id/dondon/ppmt/Main.java
@@ -2,6 +2,10 @@ package id.dondon.ppmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Main {
@@ -9,4 +13,16 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
 }
