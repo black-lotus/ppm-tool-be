0d742466ceef70381a61255d8bdbaa37331274dc
purbayanto <romdoni.purbayanto@tiket.com>
2020-04-30 22:42:42 +0700

    enable PATCH cors

--- src/main/java/id/dondon/ppmt/Main.java
+++ src/main/java/id/dondon/ppmt/Main.java
@@ -20,7 +20,7 @@ public class Main {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
             }
         };
     }
